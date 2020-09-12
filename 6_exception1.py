dccba75ff1236fa697d8a878e9e021d09294ed14
Anton K <bezzadnihnog@gmail.com>
2020-09-12 05:29:16 +0300

    task-6: done

--- 6_exception1.py
+++ 6_exception1.py
@@ -11,10 +11,22 @@
 """
 
 def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+    
+    dict = {"key": "value", "Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Дуб дерево?": "Нет.", "Динамо бежит?": "Все бегут"}
+
+    while True:
+        user_say = input('Спрашивай: ')
+
+        try:
+            if user_say in dict.keys():
+                print('value')
+                #print(dict[])
+                break
+            else:
+                print('Я не на экзамене: {}'.format(user_say))
+        except ValueError:
+            print("Пока")
+            break
     
 if __name__ == "__main__":
     ask_user()
